04a155e7a93986250c9c1ec9ec4f52e085bd2d9b
DiegoPatterson <DiegoChristopher.Patterson@calbaptist.edu>
2024-09-28 12:45:52 -0700

    For Expenses app

---
+++ expenses_app/lib/widgets/expenses.dart
@@ -0,0 +1,66 @@
+import 'package:expenses_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expenses_app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+import 'package:expenses_app/models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) => NewExpense(onAddExpense:  _addExpense),
+    );
+  }
+
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /*vvvvvvvvvvvvvvvvvvv*Important For Future apps*vvvvvvvvvvvvvvvvvvvvvvvv*/
+      appBar: AppBar(
+        title: const Text('Flutter Expense Tracker'),
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+        ],
+      ),
+      /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -37,7 +37,7 @@ class _QuizState extends State<Quiz> {
   void restartQuiz() {
     setState(() {
       _selectedAnswers = [];
-      activeScreen = 'questions-screen';
+      activeScreen = 'start-screen';
     });
   }
 
